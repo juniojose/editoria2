e7ce6469108abe4b8a124773b88385a32cfd46f8
juniojose <juniojose@gmail.com>
2025-08-14 19:36:40 -0300

    fix(usermanager): correct DSN string concatenation in generate-token

--- UserManager/generate-token.php
+++ UserManager/generate-token.php
@@ -14,7 +14,7 @@ $dotenv->load();
 function get_management_pdo(): PDO
 {
     try {
-        $dsn = "mysql:host={\$_ENV['DB_HOST']};port={\$_ENV['DB_PORT']};dbname={\$_ENV['DB_DATABASE']};charset=utf8mb4";
+        $dsn = "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_DATABASE'] . ";charset=utf8mb4";
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
